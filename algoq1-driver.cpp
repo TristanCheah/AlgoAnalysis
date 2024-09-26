305b8edb623d7d4fa3052afa2bb64ad9d3180db6
TristanCheah <cheahtristan@Hotmail.sg>
2024-09-26 16:49:57 +0800

    for owen

--- algoq1-driver.cpp
+++ algoq1-driver.cpp
@@ -3,30 +3,31 @@
 #include <vector>
 #include <iterator>
 
-struct Hospital{
+struct Hospital {
     int remainingSlots;
     char id;
     std::vector<int> prefList;
-    std::vector<int> notProposedTo;
+    
 };
-struct Student{
+struct Student {
     bool engaged;
     int id;
     std::vector<char> prefList;
+    std::vector<char> notProposedTo;
     char assignedTo;
 };
-int main(int argc, char *argv[]){
-    if (argc < 2) {
-        std::cout << "Usage: ./algoq1.out input-text-file" << std::endl;
-        return 0;
-    }
-    std::ifstream ifs {argv[1]};
-    if (!ifs) { // file opened? NO - then abort program
-        std::cerr << "Cannot open input file \"" << argv[1] << "\"" << std::endl;
-        return 0;
-    }
+int main(int argc, char* argv[]) {
+    //if (argc < 2) {
+    //    std::cout << "Usage: ./algoq1.out input-text-file" << std::endl;
+    //    return 0;
+    //}
+    //std::ifstream ifs{ argv[1] };
+    //if (!ifs) { // file opened? NO - then abort program
+    //    std::cerr << "Cannot open input file \"" << argv[1] << "\"" << std::endl;
+    //    return 0;
+    //}
 
-    std::cout << ifs.rdbuf() << "\n";
+    //std::cout << ifs.rdbuf() << "\n";
 
     //Question 1a
 
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]){
     C.prefList.push_back(4);
     C.prefList.push_back(3);
 
-    C.notProposedTo = C.prefList;
+   
 
     C.remainingSlots = 2;
 
@@ -51,28 +52,73 @@ int main(int argc, char *argv[]){
     M.prefList.push_back(2);
     M.prefList.push_back(4);
 
-    C.notProposedTo = M.prefList;
     M.remainingSlots = 2;
 
     std::vector<Hospital> hVector;
 
     hVector.push_back(M);
     hVector.push_back(C);
+
+    std::vector<Student> sVector;
+    for (int i = 1; i < 6; i++) {
+        Student s;
+        s.id = i;
+        s.engaged = false;
+        if (i < 4) {
+            s.prefList.push_back('C');
+            s.prefList.push_back('M');
+        }
+        else {
+            s.prefList.push_back('M');
+            s.prefList.push_back('C');
+        }
+        s.notProposedTo = s.prefList;
+        sVector.push_back(s);
+    }
+
     //THIS WILL BE DONE BY TEXT INPUT
-    std::vector<Hospital>::iterator it = hVector.begin();
-//HOSPITALS ARE MEN
-    while(hVector.size() > 0){
-        std::cout << it->id << " Remaining Slots: " << it->remainingSlots << "\n";
-        
-        it->remainingSlots--;
-        if (it->remainingSlots == 0){
-            hVector.erase(it);
-            it = hVector.begin();
+    std::vector<Student>::iterator currentStudent = sVector.begin();
+
+    //HOSPITALS ARE MEN
+    while (true) {
+        if (currentStudent->engaged == false && currentStudent->notProposedTo.size() > 0) {
+            for (int i = 0; i < currentStudent->notProposedTo.size(); i++) {
+                char id = currentStudent->notProposedTo[i];
+                std::vector<Hospital>::iterator hospital = std::find_if(hVector.begin(), hVector.end(), [id](Hospital h) {
+                    return h.id == id;
+                    });
+                std::vector<Student>::iterator student = std::find_if(sVector.begin(), sVector.end(),
+                    [currentStudent](Student s) {
+                    return s.id ==  currentStudent->id;
+                    });
+
+                if (hospital->remainingSlots > 0) {
+                    student->engaged = true;
+                    student->assignedTo = id;
+                    hospital->remainingSlots--;
+                    currentStudent->notProposedTo.erase(currentStudent->notProposedTo.begin() + i);
+                    break;
+                }
+                else {
+                    //currently engaged
+                    // if the hospital prefers this student
+                   
+
+
+                }
+            }
+
+        }
+
+        std::vector<Student>::iterator freeStudent = std::find_if(sVector.begin(), sVector.end(), [](Student s) {
+            return s.engaged == false && s.notProposedTo.size() > 0;
+            });
+        if (freeStudent != sVector.end()) {
+            currentStudent = freeStudent;
+        }
+        else {
+            break;
         }
     }
-    
-    
-
-
     return 0;
 }
